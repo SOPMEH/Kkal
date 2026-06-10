8c479935a0c090767de67ca70332d01122cf24dd
SOPMEH <197745230+SOPMEH@users.noreply.github.com>
2026-06-10 10:52:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kkal-v1";
+const CACHE = "kkal-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
